7a65464991be7283132c35eb6ee2a5b285bd19b0
Bao Pham Gia <paopaokk321@gmail.com>
2023-04-15 22:21:45 +0700

    ++

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const morgan = require("morgan");
+// import { handlebars } from "express-handlebars";
+const handlebars = require("express-handlebars");
+const app = express();
+const port = 3000;
+
+//HTTP loger
+app.use(morgan("combined"));
+
+//Templates engine
+app.engine("handlebars", handlebars());
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
